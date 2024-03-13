609de48cba892d33fc8d231ba044377722e85124
snahmd <sanahmed7@gmail.com>
2024-03-13 14:12:41 +0100

    user with 10 credits can create PhotoPair

--- api/views.py
+++ api/views.py
@@ -19,10 +19,12 @@ class PhotoPairViewSet(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        
-
         serializer.is_valid(raise_exception=True)
-        
+        ###
+        credit = PixelCoin.objects.get(user=request.user).credit
+        if credit < 10:
+            return Response({'error': 'You do not have enough credits to create a new photo pair'}, status=status.HTTP_400_BAD_REQUEST)
+        ####
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
