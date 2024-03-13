f00ff5f3ba72cfc6ffdbeb5279235b1a873729d7
snahmd <sanahmed7@gmail.com>
2024-03-13 14:29:18 +0100

    Increase voter credit

--- api/views.py
+++ api/views.py
@@ -37,4 +37,21 @@ class VoteViewSet(viewsets.ModelViewSet):
     queryset = Vote.objects.all()
     serializer_class = VoteSerializer
     permission_classes = [IsAuthenticated]
-    
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        ### Increase the credit of the user who voted by 1
+        
+        user = request.user
+        pixel_coin = PixelCoin.objects.get(user=user)
+        pixel_coin.credit += 1
+        pixel_coin.save()
+
+        ####
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
